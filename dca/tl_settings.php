90ed247e3b114fedefbc05cc120685accf565d27
mathContao <math-home@web.de>
2021-02-06 18:39:03 +0100

    Einstellungen: Username+Passwort für direkten Login entfernt
    Grund: die verwendete Funktion für die Verschlüsselung der Daten wurde in PHP7.2+ entfernt

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -13,7 +13,7 @@ if (!defined('TL_ROOT'))
 /**
  * Add to palette
  */
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{piwikcharts_legend:hide},piwikchartsURL,piwikchartsSiteID,piwikchartsAuthCode,piwikchartsPeriod,piwikchartsUsername,piwikchartsPassword,piwikchartsRedirect;';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{piwikcharts_legend:hide},piwikchartsURL,piwikchartsSiteID,piwikchartsAuthCode,piwikchartsPeriod,piwikchartsRedirect;';
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['piwikchartsURL'] = array
     (
@@ -47,25 +47,10 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['piwikchartsPeriod'] = array
     'eval' => array('mandatory' => false, 'rgxp' => 'natural', 'tl_class' => 'w50')
 );
 
-$GLOBALS['TL_DCA']['tl_settings']['fields']['piwikchartsUsername'] = array
-    (
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['be_piwikcharts']['username'],
-    'inputType' => 'text',
-    'exclude' => true,
-    'eval' => array('mandatory' => false, 'tl_class' => 'w50')
-);
-
-$GLOBALS['TL_DCA']['tl_settings']['fields']['piwikchartsPassword'] = array
-    (
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['be_piwikcharts']['password'],
-    'inputType' => 'text',
-    'exclude' => true,
-    'eval' => array('mandatory' => false, 'hideInput' => true, 'encrypt' => true, 'tl_class' => 'w50')
-);
-
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['piwikchartsRedirect'] = array
     (
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['be_piwikcharts']['redirect'],
     'inputType' => 'checkbox',
+    'eval' => array('tl_class' => 'clr')
 );
